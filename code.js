2d4ef84e9be1eef988f48acffc0139169fcacc57
pvrao81 <87902210+pvrao81@users.noreply.github.com>
2021-07-25 14:37:22 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+﻿var total_links = 5;
+var random_number = Math.floor((Math.random()*total_links));
+var random_link = new Array();
+random_link[0] = '<a href=https://clicks.aiplexads.com/c?o=12352037&m=2848&a=180799 style="text-decoration:none" target="_blank"> Study MBA Online from India’s Top EdTech University. Admissions Open. </a>';
+random_link[1] = '<a href=https://clicks.aiplexads.com/c?o=6980335&m=2848&a=180799 style="text-decoration:none" target="_blank"> Instant approval on Loan of up to 25 Lacs. Apply Now</a>';
+random_link[2] = '<a href=https://clicks.aiplexads.com/c?o=12355039&m=2848&a=180799 style="text-decoration:none" target="_blank"> Looking for assured income for your future? Your answer lies inside!</a>';
+random_link[3] = '<a href=https://clicks.aiplexads.com/c?o=8474330&m=2848&a=180799 style="text-decoration:none" target="_blank"> Get the best price for your car, book an appointment NOW @OlxAuto</a>';
+random_link[4] = '<a href=https://clicks.aiplexads.com/c?o=7800625&m=2848&a=180799 style="text-decoration:none" target="_blank"> Find your Life Partner on Kalyan Matrimony</a>';
+document.write(random_link[random_number]);
